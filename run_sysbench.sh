e160db47b3f22d6d0c50681c93d2ac762b783243
sudheer <sudheer@yugabyte.com>
2020-04-27 16:38:16 -0700

    Addressed script comments.

--- run_sysbench.sh
+++ run_sysbench.sh
@@ -1,21 +1,20 @@
-pgip=127.0.0.1
+pgip=$1
 port=5433
 user=yugabyte
 db=yugabyte
 run_threads=64
 table_size=1000000
 time=120
-prefx='YB_1Mil_'
 
 load_workload() {
   echo "LOADING"
-  time sysbench oltp_insert --tables=1 --table-size=$table_size --range_key_partitioning=true --serial_cache_size=1000 --db-driver=pgsql --pgsql-host=$pgip --pgsql-port=$port --pgsql-user=$user --pgsql-db=$db --threads=1 prepare > $prefx_load.txt
+  time sysbench oltp_insert --tables=1 --table-size=$table_size --range_key_partitioning=true --serial_cache_size=1000 --db-driver=pgsql --pgsql-host=$pgip --pgsql-port=$port --pgsql-user=$user --pgsql-db=$db --threads=1 prepare
   echo "DONE LOADING"
 }
 
 run_workload() {
   echo "RUNNING $1"
-  time sysbench $1 --tables=1 --table-size=$table_size --range_key_partitioning=true --serial_cache_size=1000 --db-driver=pgsql --pgsql-host=$pgip --pgsql-port=$port --pgsql-user=$user --pgsql-db=$db --threads=$run_threads --time=$time --warmup-time=120 run > $prefx$1.txt
+  time sysbench $1 --tables=1 --table-size=$table_size --range_key_partitioning=true --serial_cache_size=1000 --db-driver=pgsql --pgsql-host=$pgip --pgsql-port=$port --pgsql-user=$user --pgsql-db=$db --threads=$run_threads --time=$time --warmup-time=120 run
   echo "DONE $1"
 }
 
