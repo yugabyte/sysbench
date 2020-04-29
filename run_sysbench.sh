9d12e618a37567876844dcb1ffea04e0983f2050
sudheer <sudheer@yugabyte.com>
2020-04-28 23:51:52 -0700

    Modify the run script to better reflect our docs.
    Reviewers
    Neha, Karhik

--- run_sysbench.sh
+++ run_sysbench.sh
@@ -3,22 +3,24 @@ port=5433
 user=yugabyte
 db=yugabyte
 run_threads=64
-table_size=1000000
-time=120
+table_size=100000
+num_tables=10
+time=60
 
-load_workload() {
-  echo "LOADING"
-  time sysbench oltp_insert --tables=1 --table-size=$table_size --range_key_partitioning=true --serial_cache_size=1000 --db-driver=pgsql --pgsql-host=$pgip --pgsql-port=$port --pgsql-user=$user --pgsql-db=$db --threads=1 prepare
-  echo "DONE LOADING"
+sqlsh=/repositories/yugabyte-db/bin/ysqlsh
+
+delete_tables() {
+  for i in `seq $num_tables`; do $sqlsh -h $pgip -c "drop table sbtest$i;"; done
 }
 
 run_workload() {
   echo "RUNNING $1"
-  time sysbench $1 --tables=1 --table-size=$table_size --range_key_partitioning=true --serial_cache_size=1000 --db-driver=pgsql --pgsql-host=$pgip --pgsql-port=$port --pgsql-user=$user --pgsql-db=$db --threads=$run_threads --time=$time --warmup-time=120 run
+  time sysbench oltp_insert --tables=$num_tables --table-size=$table_size --range_key_partitioning=true --serial_cache_size=1000 --db-driver=pgsql --pgsql-host=$pgip --pgsql-port=$port --pgsql-user=$user --pgsql-db=$db --threads=1 prepare
+  time sysbench $1 --tables=$num_tables --table-size=$table_size --range_key_partitioning=true --serial_cache_size=1000 --db-driver=pgsql --pgsql-host=$pgip --pgsql-port=$port --pgsql-user=$user --pgsql-db=$db --threads=$run_threads --time=$time --warmup-time=120 run
+  delete_tables
   echo "DONE $1"
 }
 
-load_workload
 run_workload oltp_insert
 run_workload oltp_point_select
 run_workload oltp_write_only
