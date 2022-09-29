357baaa4110f8ce09d05a88b322fcffa31e99f08
mangesh-at-yb <98325393+mangesh-at-yb@users.noreply.github.com>
2022-09-29 15:37:59 +0530

    added nohup
    added nohup

--- pre_and_post_scrs/psql.sh
+++ pre_and_post_scrs/psql.sh
@@ -2,6 +2,6 @@
 echo -e " ======= `date`:: Pre-load script called ...... ======== " > /tmp/psql_sh.log
 for ybsrv in `echo ${1} | tr "," " "`;
 do
-   echo "PGPASSWORD=${2} /usr/bin/psql -h ${ybsrv} -p 5433 -U yugabyte -c \"SELECT pg_sleep(${3}), 'Pre-LOAD Sleep done...' as msg;\" &" >> /tmp/psql_sh.log
-   PGPASSWORD=${2} /usr/bin/psql -h ${ybsrv} -p 5433 -U yugabyte -c "SELECT pg_sleep(${3}), 'Pre-LOAD Sleep done...' as msg;" >> /tmp/psql_sh.log & 2>&1
+   echo "PGPASSWORD=${2} nohup /usr/bin/psql -h ${ybsrv} -p 5433 -U yugabyte -c \"SELECT pg_sleep(${3}), 'Pre-LOAD Sleep done...' as msg;\" &" >> /tmp/psql_sh.log
+   PGPASSWORD=${2} nohup /usr/bin/psql -h ${ybsrv} -p 5433 -U yugabyte -c "SELECT pg_sleep(${3}), 'Pre-LOAD Sleep done...' as msg;" >> /tmp/psql_sh.log & 2>&1
 done
