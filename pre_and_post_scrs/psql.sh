2dc1b609f90e8733d003d2a369d83c08cebcae5b
mangesh-at-yb <98325393+mangesh-at-yb@users.noreply.github.com>
2022-09-29 12:16:35 +0530

    increased sleep time to 5 min. to keep db conn
    increased sleep time to 5 min. to keep db conn live for 5 min

--- pre_and_post_scrs/psql.sh
+++ pre_and_post_scrs/psql.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 echo -e " ======= In script ...... ======== "
-echo "PGPASSWORD=${2} /usr/bin/psql -h ${1} -p 5433 -U yugabyte -c \"SELECT pg_sleep(1), 'Pre-LOAD Sleep done...' as msg;\" &"
-PGPASSWORD=${2} /usr/bin/psql -h ${1} -p 5433 -U yugabyte -c "SELECT pg_sleep(1), 'Pre-LOAD Sleep done...' as msg;" &
+echo "PGPASSWORD=${2} /usr/bin/psql -h ${1} -p 5433 -U yugabyte -c \"SELECT pg_sleep(300), 'Pre-LOAD Sleep done...' as msg;\" &"
+PGPASSWORD=${2} /usr/bin/psql -h ${1} -p 5433 -U yugabyte -c "SELECT pg_sleep(300), 'Pre-LOAD Sleep done...' as msg;" &
