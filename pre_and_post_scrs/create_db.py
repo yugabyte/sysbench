5652d144abb89097f6c6779c312b8d0a7d9465cd
Cloud User <centos@ip-10-9-207-41.us-west-2.compute.internal>
2023-10-04 09:01:03 +0000

    Fixed password issue

--- pre_and_post_scrs/create_db.py
+++ pre_and_post_scrs/create_db.py
@@ -2,7 +2,7 @@ import psycopg2
 import argparse
 
 def create_db(arg):
-    if len(arg.pwd) == 0:
+    if arg.pwd and len(arg.pwd) == 0:
         conn = psycopg2.connect("host={} dbname=yugabyte user=yugabyte port=5433".format(arg.db))
     else:
         conn = psycopg2.connect("host={} dbname=yugabyte user=yugabyte password={} port=5433".format(arg.db, arg.pwd))
@@ -17,7 +17,7 @@ def create_db(arg):
 def main():
     parser = argparse.ArgumentParser(description='Enter the Yugabyte Cluster IP, Username, Number of databases to be created')
     parser.add_argument("-d", type=str, action="store", dest="db", required=True)
-    parser.add_argument("-p", type=str, action="store", dest="pwd", required=True)
+    parser.add_argument("-p", type=str, action="store", dest="pwd", required=False)
     parser.add_argument("-n", type=int, action="store", dest="num", required=True)
     args = parser.parse_args()
     create_db(args)
