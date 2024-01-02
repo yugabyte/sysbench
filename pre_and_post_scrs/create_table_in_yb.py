e28ca140161b70c5712dfe42200558a0bfa9dc5f
Shantanu Gupta <sgupta@yugabyte.com>
2024-01-02 13:04:41 -0600

    fixed script with correct table details

--- pre_and_post_scrs/create_table_in_yb.py
+++ pre_and_post_scrs/create_table_in_yb.py
@@ -1,17 +1,17 @@
 import psycopg2
 import argparse
 
-def create_tables(arg,i):
+def create_tables(arg):
     conn = None
     try:
         if arg.pwd and len(arg.pwd) == 0:
-            conn = psycopg2.connect("host={} dbname=yugabyte user=yugabyte port=5433".format(arg.db, i))
+            conn = psycopg2.connect("host={} dbname=yugabyte user=yugabyte port=5433".format(arg.db))
         else:
             conn = psycopg2.connect(
-                "host={} dbname=yugabyte user=yugabyte password={} port=5433".format(arg.db, i, arg.pwd))
+                "host={} dbname=yugabyte user=yugabyte password={} port=5433".format(arg.db, arg.pwd))
         conn.set_session(autocommit=True)
         cur = conn.cursor()
-        for i in range(1, args.num + 1):
+        for i in range(1, arg.num + 1):
             print("Creating table perdb_table_{} in DB yugabyte".format(i))
             cur.execute("CREATE TABLE perdb_table_{}(col1 int, col2 int, primary key(col1))".format(i))
     except psycopg2.DatabaseError as error:
@@ -26,7 +26,7 @@ def main():
     parser.add_argument("-p", type=str, action="store", dest="pwd", required=False)
     parser.add_argument("-n", type=int, action="store", dest="num", required=True)
     args = parser.parse_args()
-    create_tables(args, i)
+    create_tables(args)
 
 if __name__ == "__main__":
     main()
